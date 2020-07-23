1711aba002282a2b2562abf10cdd11204bc9563a
Will Pearson <will@mcneel.com>
2020-07-23 19:05:29 +0100

    Add cpu min, max and memory limit

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.CodeDom;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace JobObjectInfo
 {
@@ -27,23 +21,21 @@ namespace JobObjectInfo
             int nLength = Marshal.SizeOf(typeof(JOBOBJECT_CPU_RATE_CONTROL_INFORMATION));
             IntPtr pJobpil = Marshal.AllocHGlobal(nLength);
 
-            bool bRet = QueryInformationJobObject(hJob, JOBOBJECTINFOCLASS.JobObjectCpuRateControlInformation, pJobpil, nLength, out _);
-
             Console.WriteLine("Job Object CPU rate control information:");
 
-            if (bRet)
+            if (QueryInformationJobObject(hJob, JOBOBJECTINFOCLASS.JobObjectCpuRateControlInformation, pJobpil, nLength, out _))
             {
                 var info = (JOBOBJECT_CPU_RATE_CONTROL_INFORMATION)Marshal.PtrToStructure(pJobpil, typeof(JOBOBJECT_CPU_RATE_CONTROL_INFORMATION));
 
                 Console.WriteLine($"  CPU rate: {info.CpuRate}");
-                //Console.WriteLine($"  Max rate: {info.MaxRate}"); // not implemented
-                //Console.WriteLine($"  Min rate: {info.MinRate}"); // not implemented
                 Console.WriteLine($"  Weight: {info.Weight}");
+                Console.WriteLine($"  Min rate: {info.MinMax.MinRate}"); // not implemented
+                Console.WriteLine($"  Max rate: {info.MinMax.MaxRate}"); // not implemented
                 Console.WriteLine($"  Control flags: {info.ControlFlags}");
-                Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_ENABLE:       {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_ENABLE));
-                Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_WEIGHT_BASED: {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_WEIGHT_BASED));
-                Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_HARD_CAP:     {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_HARD_CAP));
-                Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_MIN_MAX_RATE: {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_MIN_MAX_RATE));
+                //Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_ENABLE:       {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_ENABLE));
+                //Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_WEIGHT_BASED: {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_WEIGHT_BASED));
+                //Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_HARD_CAP:     {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_HARD_CAP));
+                //Console.WriteLine("    JOB_OBJECT_CPU_RATE_CONTROL_MIN_MAX_RATE: {0}", info.ControlFlags.HasFlag(CpuFlags.JOB_OBJECT_CPU_RATE_CONTROL_MIN_MAX_RATE));
 
                 // JOBOBJECT_CPU_RATE_CONTROL_INFORMATION.CpuRate
                 // 
@@ -65,6 +57,24 @@ namespace JobObjectInfo
                 var msg = "ERROR: failed to get CPU rate control information for the job object associated with the calling process\n" +
                           "       (it might not exist...)";
                 Console.WriteLine(msg);
+            }
+
+            // get job object memory limit (via extended limit information)
+
+            nLength = Marshal.SizeOf(typeof(JOBOBJECT_EXTENDED_LIMIT_INFORMATION));
+            pJobpil = Marshal.AllocHGlobal(nLength);
+
+            Console.WriteLine("\nJob Object extended limit information:");
+
+            if (QueryInformationJobObject(hJob, JOBOBJECTINFOCLASS.JobObjectExtendedLimitInformation, pJobpil, nLength, out _))
+            {
+                var extended_info = (JOBOBJECT_EXTENDED_LIMIT_INFORMATION)Marshal.PtrToStructure(pJobpil, typeof(JOBOBJECT_EXTENDED_LIMIT_INFORMATION));
+                Console.WriteLine($"  Job memory limit: {extended_info.JobMemoryLimit}");
+                Console.WriteLine($"  Process memory limit: {extended_info.ProcessMemoryLimit}");
+            }
+            else
+            {
+                Console.WriteLine("ERROR");
             }
 
             Console.WriteLine($"\nTotal cpu 'cores': {Environment.ProcessorCount}");
@@ -96,8 +106,86 @@ namespace JobObjectInfo
             public UInt32 CpuRate;
             [FieldOffset(4)]
             public UInt32 Weight;
+            [FieldOffset(4)]
+            public DummyStruct MinMax;
+            //[FieldOffset(8)]
+            //public UInt16 MinRate;
+            //[FieldOffset(12)]
+            //public UInt16 MaxRate;
         }
 
+        [StructLayout(LayoutKind.Sequential)]
+        struct DummyStruct
+        {
+            public UInt16 MinRate;
+            public UInt16 MaxRate;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct JOBOBJECT_EXTENDED_LIMIT_INFORMATION
+        {
+            public JOBOBJECT_BASIC_LIMIT_INFORMATION BasicLimitInformation;
+            public IO_COUNTERS IoInfo;
+            public UIntPtr ProcessMemoryLimit;
+            public UIntPtr JobMemoryLimit;
+            public UIntPtr PeakProcessMemoryUsed;
+            public UIntPtr PeakJobMemoryUsed;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct IO_COUNTERS
+        {
+            public ulong ReadOperationCount;
+            public ulong WriteOperationCount;
+            public ulong OtherOperationCount;
+            public ulong ReadTransferCount;
+            public ulong WriteTransferCount;
+            public ulong OtherTransferCount;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct JOBOBJECT_BASIC_LIMIT_INFORMATION
+        {
+            public Int64 PerProcessUserTimeLimit;
+            public Int64 PerJobUserTimeLimit;
+            public JOBOBJECTLIMIT LimitFlags;
+            public UIntPtr MinimumWorkingSetSize;
+            public UIntPtr MaximumWorkingSetSize;
+            public UInt32 ActiveProcessLimit;
+            public Int64 Affinity;
+            public UInt32 PriorityClass;
+            public UInt32 SchedulingClass;
+        }
+
+        [Flags]
+        public enum JOBOBJECTLIMIT : uint
+        {
+            // Basic Limits
+            Workingset = 0x00000001,
+            ProcessTime = 0x00000002,
+            JobTime = 0x00000004,
+            ActiveProcess = 0x00000008,
+            Affinity = 0x00000010,
+            PriorityClass = 0x00000020,
+            PreserveJobTime = 0x00000040,
+            SchedulingClass = 0x00000080,
+
+            // Extended Limits
+            ProcessMemory = 0x00000100,
+            JobMemory = 0x00000200,
+            DieOnUnhandledException = 0x00000400,
+            BreakawayOk = 0x00000800,
+            SilentBreakawayOk = 0x00001000,
+            KillOnJobClose = 0x00002000,
+            SubsetAffinity = 0x00004000,
+
+            // Notification Limits
+            JobReadBytes = 0x00010000,
+            JobWriteBytes = 0x00020000,
+            RateControl = 0x00040000,
+        }
+
+        [Flags]
         public enum CpuFlags
         {
             JOB_OBJECT_CPU_RATE_CONTROL_ENABLE = 0x00000001,
